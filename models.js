e2f2f3201bb98522743591bd3d3988038cb37854
Dan Kang <kang@flitto.com>
2013-09-21 17:05:22 -0700

    add some apis

--- models.js
+++ models.js
@@ -1,4 +1,6 @@
-var mysql      = require('mysql');
+var mysql = require('mysql')
+  , Step = require('step')
+  , _ = require('underscore');
 
 var pool = mysql.createPool({
   host     : 'localhost',
@@ -7,11 +9,149 @@ var pool = mysql.createPool({
   database : 'tunited'
 });
 
-exports.get = function (options, callback) {
+exports.getUser = function (options, callback) {
+  var sql = 'SELECT user_id, id, name, profile_photo FROM users WHERE user_id='
+    + mysql.escape(options.user_id);
+
   pool.getConnection(function(err, conn){
-    conn.query('SELECT NOW()', function (err, rows) {
+    conn.query(sql, function (err, rows) {
       conn.release();
       callback(err, rows);
     });
   })
 };
+
+
+exports.addList = function (options, callback) {
+  var sql = 'INSERT INTO lists SET ?';
+  var post = { user_id: options.user_id, folder_name: options.folder_name };
+
+  pool.getConnection(function(err, conn){
+    conn.query(sql, post, function (err, result) {
+      conn.release();
+      callback(err, { list_id: result.insertId });
+    });
+  })
+};
+
+exports.getLists = function (options, callback) {
+  var sql = 'SELECT * FROM (SELECT * FROM lists WHERE user_id=' + mysql.escape(options.user_id) + ') l'
+    + ' LEFT JOIN listitems USING(list_id) LEFT JOIN songs USING(song_id)';
+
+  pool.getConnection(function(err, conn){
+    conn.query(sql, function (err, results) {
+      conn.release();
+      callback(err, renderLists(results));
+    });
+  })
+};
+
+exports.getList = function (options, callback) {
+  var sql = 'SELECT * FROM (SELECT * FROM lists WHERE list_id=' + mysql.escape(options.list_id) + ') l'
+    + ' LEFT JOIN listitems USING(list_id) LEFT JOIN songs USING(song_id)';
+
+  pool.getConnection(function(err, conn){
+    conn.query(sql, function (err, results) {
+      conn.release();
+      callback(err, renderList(results[0]));
+    });
+  })
+};
+
+exports.deleteList = function (options, callback) {
+
+  pool.getConnection(function(err, conn){
+    Step(
+      function deleteSongs() {
+        var sql = 'DELETE FROM listitems WHERE user_id=' + mysql.escape(options.user_id)
+          + ' AND list_id=' + mysql.escape(options.list_id);
+
+        conn.query(sql, this);
+      },
+      function deleteList(err) {
+        var sql = 'DELETE FROM lists WHERE user_id=' + mysql.escape(options.user_id)
+          + ' AND list_id=' + mysql.escape(options.list_id);
+
+        conn.query(sql, this);
+      },
+      function end(err) {
+        conn.release();
+        callback(err, {});
+      }
+    );
+  });
+};
+
+exports.addSong = function (options, callback) {
+
+  pool.getConnection(function(err, conn){
+    Step(
+      function selectListitem() {
+        var sql = 'SELECT * FROM listitems WHERE list_id=' + mysql.escape(options.list_id)
+          + ' AND song_id=' + mysql.escape(options.song_id);
+
+        conn.query(sql, this);
+      },
+      function insert(err, data) {
+        if (data.length)
+          return null;
+
+        var sql = 'INSERT INTO listitems SET ?';
+        var post= { list_id: options.list_id, song_id: options.song_id, user_id: options.user_id };
+
+        conn.query(sql, this);
+      },
+      function end() {
+        conn.release();
+        callback(err, {});
+      }
+    );
+  });
+};
+
+exports.deleteSong = function (options, callback) {
+  var sql = 'DELETE FROM listitems WHERE list_id=' + mysql.escape(options.list_id)
+    + ' AND song_id=' + mysql.escape(options.song_id);
+
+  pool.getConnection(function(err, conn){
+    conn.query(sql, function (err, results) {
+      conn.release();
+      callback(err, {});
+    });
+  });
+};
+
+function renderLists(rows) {
+  var data = [];
+
+  rows.forEach(function(r){
+    var found = _.find(data, function(d){ return d.list_id == r.list_id; });
+
+    if (found) {
+      found.song.push(renderSong(r));
+    } else {
+      data.push(renderList(r));
+    }
+  });
+
+  return data;
+}
+
+function renderList(row) {
+  return {
+    list_id: row.list_id,
+    folder_name: row.folder_name,
+    songs: row.song_id? [ renderSong(row) ] : []
+  }
+}
+
+function renderSong(row) {
+  return {
+    song_id: row.song_id,
+    title: row.title,
+    description: row.description,
+    thumb_url: row.thumb_url,
+    youtube_url: row.youtube_url,
+    play_count: row.play_count
+  }
+}
--- routes.js
+++ routes.js
@@ -3,9 +3,113 @@ var models = require('./models');
 exports.routes = function (app) {
   var PRE = '/api';
 
-  app.get(PRE + '/', function (req, res, next) {
-    models.get({}, function (err, data) {
-      res.send(data);
-    });
-  });
+  app.get(PRE + '/users', login);
+  app.post(PRE + '/users', login);
+
+  // list api
+  app.post(PRE + '/lists', postLists);
+  app.get(PRE + '/lists', getLists);
+  app.get(PRE + '/lists/:list_id', getList);
+  app.put(PRE + '/lists/:list_id', putLists);
+  app.delete(PRE + '/lists/:list_id', deleteLists);
+  app.post(PRE + '/lists/:list_id', postListsSong);
+  app.delete(PRE + '/lists/:list_id/:song_id', deleteListsSong);
 };
+
+function login(req, res, next) {
+  var user_id = req.param('user_id');
+
+  if (!user_id)
+    return res.send(400);
+
+  models.getUser({ user_id: user_id }, function (err, data) {
+    req.session.user_id = user_id;
+    res.json(data[0]);
+  });
+}
+
+function postLists(req, res, next) {
+  var options = {
+    user_id: req.session.user_id,
+    folder_name: req.param('folder_name')
+  };
+
+  if (!options.user_id || !options.folder_name)
+    return res.send(400);
+
+  models.addList(options, function (err, data) {
+    if (err) return res.send(500, err);
+
+    res.json(data);
+  });
+}
+
+function getLists(req, res, next) {
+  var options = {
+    user_id: req.session.user_id
+  };
+
+  models.getLists(options, function (err, data) {
+    if (err) return res.send(500, err);
+
+    res.json(data);
+  });
+}
+
+function getList(req, res, next) {
+  var options = {
+    user_id: req.session.user_id,
+    list_id: req.param('list_id')
+  };
+
+  models.getList(options, function (err, data) {
+    if (err) return res.send(500, err);
+
+    res.json(data);
+  });
+}
+
+function putLists(req, res, next) {
+
+}
+
+function deleteLists(req, res, next) {
+  var options = {
+    user_id: req.session.user_id,
+    list_id: req.param('list_id')
+  };
+
+  models.deleteList(options, function (err, data) {
+    if (err) return res.send(500, err);
+
+    res.json(data);
+  });
+}
+
+function postListsSong(req, res, next) {
+  var options = {
+    user_id: req.session.user_id,
+    list_id: req.param('list_id'),
+    song_id: req.param('song_id')
+  };
+
+  models.addSong(options, function (err, data) {
+    if (err) return res.send(500, err);
+
+    res.json(data);
+  });
+}
+
+function deleteListsSong(req, res, next) {
+  var options = {
+    user_id: req.session.user_id,
+    list_id: req.param('list_id'),
+    song_id: req.param('song_id')
+  };
+
+  models.deleteSong(options, function (err, data) {
+    if (err) return res.send(500, err);
+
+    res.json(data);
+  });
+}
